a4a5987e37592871f51a4857eb38f3b13be12f36
BrianV1981 <brianv1981@users.noreply.github.com>
2026-06-17 19:21:37 -0400

    Fix: Add missing untracked cfgFunctions.hpp for ticketing

---
+++ arma3mercenaries_2026.Altis/arma3mercenaries/ticketing/cfgFunctions.hpp
@@ -0,0 +1,10 @@
+class ticketing
+{
+    tag = "A3M";
+    class ticketing_functions
+    {
+        file = "arma3mercenaries\ticketing";
+        class submitTicket {};
+        class serverLogTicket {};
+    };
+};
